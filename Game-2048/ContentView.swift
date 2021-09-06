21477e3dc5ee2744c44d67ffd8214574e6e2a7d2
13773753970 <32631243+13773753970@users.noreply.github.com>
2021-09-06 22:00:10 +0800

    Initial Commit

---
+++ Game-2048/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game-2048
+//
+//  Created by zhoupeng on 2021/9/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game-2048/Game_2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Game_2048App.swift
+//  Game-2048
+//
+//  Created by zhoupeng on 2021/9/6.
+//
+
+import SwiftUI
+
+@main
+struct Game_2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
